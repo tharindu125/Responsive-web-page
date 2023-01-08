dfd8c639bb998002bf77bf7569063913c91d8d20
tharindu125 <nawanjanaenda@gmail.com>
2023-01-09 01:38:38 +0530

    edit

--- src/Components/Pages/Navbar.js
+++ src/Components/Pages/Navbar.js
@@ -9,11 +9,11 @@ export default function Navbar() {
     <Router>
     <div>
     
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <a class="navbar-brand" href="./">
             <img src="logo.png" width="60" height="35" alt=""/>
         </a>
-        <a class="navbar-brand" href="./">Navbar</a>
+        <a class="navbar-brand" href="./">Sport HUB</a>
 
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
