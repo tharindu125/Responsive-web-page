7ec8890368186ee526d8bdcdd6d2d044d32c7f50
tharindu125 <nawanjanaenda@gmail.com>
2023-01-09 22:35:21 +0530

    edit

--- src/Components/Pages/Login.js
+++ src/Components/Pages/Login.js
@@ -1,9 +1,53 @@
 import React from 'react'
 
 export default function Login() {
-  return (
-    <div>
-        ffff
-    </div>
+  return ( 
+      
+        <div className='container' style={{display: 'flex'}}>
+        <div className='col-6'>
+          <form class="px-5 py-4 " style={{border:'1px solid black', marginTop:'5rem'}}>
+            
+            <h1 className='text-center' style={{color:"green", fontSize:'2rem', margin:'12px', fontFamily:'sans-serif', fontWeight:'800'}}>Login</h1>
+
+            <div class="form-group">
+              <label for="exampleDropdownFormEmail1">Email address</label>
+              <input type="email" class="form-control" id="exampleDropdownFormEmail1" placeholder="email@example.com"/>
+            </div>
+
+            <div class="form-group">
+              <label for="exampleDropdownFormPassword1">Password</label>
+              <input type="password" class="form-control" id="exampleDropdownFormPassword1" placeholder="Password"/>
+            </div>
+
+            <div class="form-check">
+              <input type="checkbox" class="form-check-input" id="dropdownCheck"/>
+              <label class="form-check-label" for="dropdownCheck"> Remember me</label>
+            </div>
+
+            <br/>
+            <button type="submit" class="btn btn-primary ">Login</button>
+
+            <br/><br/>
+            <div className='text-right'>
+
+              <label>New around here?<a href=".SignUp.js"> Sign up</a></label>
+              <br/>
+              <a href=".">Forgot password?</a>
+
+            </div>
+
+          </form>
+          </div>
+
+          <div className='col-6'>
+            <img src='login.png' alt=''  style={{width:'600px', height:'600px'}}/>
+            
+          </div>
+
+          
+            
+
+        </div>
+
   )
 }
--- src/Components/Pages/SignUp.js
+++ src/Components/Pages/SignUp.js
@@ -2,57 +2,35 @@ import React from 'react'
 
 export default function SignUp() {
   return (
-    <div className='container' style={{marginTop: '20px'}}>
+    <div className='container' style={{marginTop: '5rem'}}>
 
-        <form>
-
-            <div class="form-row">
+        <form className="px-5 py-4" style={{border:'1px solid black',}}>
+            
+            <h1 className='text-center' style={{color:"green", fontSize:'2rem', margin:'12px', fontFamily:'sans-serif', fontWeight:'800'}}>Register Our servise and get amazing benefits</h1>
+            <div class="form-row" style={{marginTop:'40px'}}>
 
                 <div class="form-group col-md-6">
-                    <label for='inputFirstName'>First Name</label>
+                    <label for='inputFirstName'>First Name :</label>
                     <input type="text" class="form-control" placeholder="First name"/>
                 </div>
                 <div class="form-group col-md-6">
-                    <label for ='inputLastName'>Last Name</label>
+                    <label for ='inputLastName'>Last Name :</label>
                     <input type="text" class="form-control" placeholder="Last name"/>
                 </div>
 
                 <div class="form-group col-md-6">
-                    <label for="inputEmail4">Email</label>
+                    <label for="inputEmail4">Email :</label>
                     <input type="email" class="form-control" id="inputEmail4" placeholder="Email"/>
                 </div>
                 <div class="form-group col-md-6">
-                    <label for="inputPassword4">Password</label>
+                    <label for="inputPassword4">Password :</label>
                     <input type="password" class="form-control" id="inputPassword4" placeholder="Password"/>
                 </div>
             </div>
 
             <div class="form-group">
-                <label for="inputAddress">Address</label>
+                <label for="inputAddress">Address :</label>
                 <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St"/>
-            </div>
-
-            <div class="form-group">
-                <label for="inputAddress2">Address 2</label>
-                <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor"/>
-            </div>
-
-            <div class="form-row">
-                <div class="form-group col-md-6">
-                    <label for="inputCity">City</label>
-                    <input type="text" class="form-control" id="inputCity"/>
-                </div>
-                <div class="form-group col-md-4">
-                    <label for="inputState">State</label>
-                    <select id="inputState" class="form-control">
-                        <option selected>Choose...</option>
-                        <option>...</option>
-                    </select>
-                </div>
-                <div class="form-group col-md-2">
-                    <label for="inputZip">Zip</label>
-                    <input type="text" class="form-control" id="inputZip"/>
-                </div>
             </div>
 
             <div class="form-group">
