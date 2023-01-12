ea07cff10f69786fc77b04b28511ab4214c8137b
tharindu125 <nawanjanaenda@gmail.com>
2023-01-12 23:45:23 +0530

    edit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Footer from './Components/Pages/Footer';
 import Navbar from './Components/Pages/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/Components/Pages/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+        <nav className='navbar navbar-expand-lg bg-primary'>
+            <h1>Tharindu Ranaweera</h1>
+
+        </nav>
+    </div>
+  )
+}
